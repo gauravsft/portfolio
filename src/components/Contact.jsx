8ffeb9bf02c5c76de82c3dbb2aba87c1bcd586c2
gauravsft <gauravsft@gmail.com>
2022-07-28 17:32:38 +0530

    fully done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,53 +1,33 @@
 import React from "react";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsTelephone } from "react-icons/bs";
+import './Project.css';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import {BsWhatsapp} from 'react-icons/bs'
-import './projuct.css';
 
 const Contact = () => {
+  
   return (
     <div
       name="contact"
       className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
+
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="pb-5">
+        <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Contact
+          Get in Touch
           </p>
-          <p className="py-6">Submit the form below to get in touch with me</p>
-        </div>
-
-        <div className=" flex justify-center items-center">
-          <form
-            action="https://getform.io/f/66183ff0-e8c5-4c69-acea-99195502221f"
-            method="POST"
-            className=" flex flex-col w-full md:w-1/2"
-          >
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter your email"
-              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
-            <textarea
-              name="message"
-              placeholder="Enter your message"
-              rows="5"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            ></textarea>
-
-            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-              Let's talk
-            </button>
-          </form>
-        </div>
-        <div className="contact-container">
+          <div className="to-gray-800 pt-10 contact-ele-own ">
+            <div className="flex space-x-2.5">
+            <HiOutlineMail size={40} />
+            <p className="text-2xl">Email : gauravsft@gmail.com</p>
+            </div>
+           <div className="flex space-x-2.5 py-6">
+           <BsTelephone size={40} />
+            <p className="text-2xl">Mobile No : 7488585702</p>
+           </div>
+           <div className="contact-container">
              <a className="icon" href="https://www.linkedin.com/in/gauravsft/"  target="_blank"
               rel="noreferrer"
              
@@ -71,6 +51,41 @@ const Contact = () => {
               <p>WhatsApp</p>
              </a>
            </div>
+
+
+
+          </div>
+          {/* <p className="py-6 underline underline-offset-1 ">Submit the form below to get in touch with me</p>
+        </div>
+        <div className=" flex justify-center items-center">
+          <form
+            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter your email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's talk
+            </button>
+          </form> */}
+        </div>
       </div>
     </div>
   );
